eaaa0fcb18ef3adb73260008521de34f40b6d33e
Norton Glitz <norton.glitz@gmail.com>
2023-10-18 01:31:37 -0300

    Github login and avatar image working

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -101,7 +101,7 @@ const LoginModal = () => {
                 outline
                 label="Continue with Github"
                 icon={AiFillGithub}
-                onClick={() => { }}
+                onClick={() => signIn('github')}
             />
             <div
                 className="
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -15,6 +15,7 @@ import Heading from '@/app/components/Heading'
 import Input from '@/app/components/inputs/Input'
 import Button from '@/app/components/Button'
 import toast from 'react-hot-toast'
+import { signIn } from 'next-auth/react'
 
 
 const RegisterModal = () => {
@@ -98,7 +99,7 @@ const RegisterModal = () => {
                 outline
                 label="Continue with Github"
                 icon={AiFillGithub}
-                onClick={() => { }}
+                onClick={() => signIn('github')}
             />
             <div
                 className="
