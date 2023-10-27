184a3b1d944d098a8cb2c009c5632a595eda4c06
Norton Glitz <norton.glitz@gmail.com>
2023-10-26 22:31:13 -0300

    Fix type error on deploy

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextResponse } from "next/server"
 
 import prisma from '@/app/libs/prismadb'
 import getCurrentUser from "@/app/actions/getCurrentUser"
 
 export async function POST(
-    request: Response | NextRequest
+    request: Request
 ) {
     const currentUser = await getCurrentUser()
 
